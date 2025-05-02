a18cbad6d39c15998d0869f41161a9e065dfb0c5
KvDevX <kvdex9258@gmail.com>
2025-05-02 06:25:23 -0300

    Update and rename FE.lua to Fe.lua

--- Fe.lua
+++ Fe.lua
@@ -73432,6 +73432,9 @@ gamesse:AddButton({
 	Title = "FE Crash/Lag Sount Bronx",
 	Description = ":)",
 	Callback = function()
+		if game.PlaceId ~= 10179538382 then
+            game:GetService("TeleportService"):Teleport(10179538382,game:GetService("Players").LocalPlayer)
+        else
             game:GetService("RunService").RenderStepped:Connect(function()
                 local args = {
                     [1] = "None",
@@ -73490,6 +73493,8 @@ gamesse:AddButton({
                     end)
                 end)
             end)
+        end
+	end
 })
 local Chatwithhackers = Window:AddTab({
 	Title = "Chat with other exploiters",
